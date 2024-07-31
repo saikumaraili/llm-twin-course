06dd8812681ca27e06a8b4828633180519c0ff02
saikumaraili <sk.saikumaraili@gmail.com>
2024-07-31 09:38:37 -0400

    added logging info

--- course/module-1/test-github.py
+++ course/module-1/test-github.py
@@ -1,3 +1,4 @@
+'''
 from unittest import TestCase, mock
 from crawlers.github import GithubCrawler
 
@@ -14,3 +15,21 @@ class TestGithubCrawlerSimplified(TestCase):
 		self.assertTrue(mock_save.called)
 		# Optionally, check if save was called with expected data
 		# self.assertEqual(mock_save.call_args[0][0], expected_data)
+
+'''
+import logging
+from crawlers.github import GithubCrawler
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    handlers=[
+                        logging.FileHandler("debug.log"),
+                        logging.StreamHandler()
+                    ])
+
+def test_extract():
+	# Instantiate GithubCrawler
+	logging.info("Starting test_extract")
+	crawler = GithubCrawler()
+	
+	# Call extract method with a sample GitHub URL
+	crawler.extract(link='https://github.com/saikumaraili/job-probability.git', user='saikumaraili')
